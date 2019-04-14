0becebc4542f4d0ee912ba70f6ec658b4fb47178
leixus <1442526564@qq.com>
2019-04-14 13:22:53 +0800

    搜索页,页面完成

--- src/script/controller/searchCtrl.js
+++ src/script/controller/searchCtrl.js
@@ -1,7 +1,34 @@
 "use strict";
 
-angular.module('app').controller("searchCtrl",
-    function ($scope) {
-        $scope.myName = "names"
-    }
+angular.module('app').controller("searchCtrl", ["$http", "$scope",
+        function ($http, $scope) {
+            $http.get('data/positionList.json')
+                    .success(function(resp){
+                        $scope.list = resp;
+                    });
+            $scope.sheet = {
+                'list': [
+                    {
+                        name: '不限'
+                    },
+                    {
+                        name: '少于50人'
+                    },
+                    {
+                        name: '少于50人'
+                    },
+                    {
+                        name: '50~100人'
+                    },
+                    {
+                        name: '100~500人'
+                    },
+                    {
+                        name: '500人以上'
+                    }
+                ]
+            };
+
+        }
+    ]
 );
